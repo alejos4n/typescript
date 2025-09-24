22f7d0002c344dde1eeb02cb56fea292a09580a5
alejos4n <alejo-lopera@hotmail.com>
2025-09-23 23:35:37 -0500

    arrehlos

---
+++ tipoarrays.js
@@ -0,0 +1,12 @@
+//los arreglos pueden ser usando [] llamado arreglo tipo definido
+//el siguiente arreglo solo puede tener números
+var listaDeNumeros = [1, 2, 3];
+//listaDeNumeros.push("a"); //esto dará error
+//otra forma de crear un arreglo es usando Array<number>
+var listaDeNumerosdos = [1, 2, 3];
+//imprimo arreglo con []
+console.log("arreglo numerico[]");
+console.log(listaDeNumeros);
+//lista con Array<>
+console.log("array<number>");
+console.log(listaDeNumerosdos);
---
+++ tipoarrays.ts
@@ -0,0 +1,13 @@
+//los arreglos pueden ser usando [] llamado arreglo tipo definido
+//el siguiente arreglo solo puede tener números
+let listaDeNumeros: number[] = [1, 2, 3];
+//listaDeNumeros.push("a"); //esto dará error
+
+//otra forma de crear un arreglo es usando Array<number>
+let listaDeNumerosdos: Array<number> = [1, 2, 3];
+//imprimo arreglo con []
+console.log("arreglo numerico[]");
+console.log(listaDeNumeros);
+//lista con Array<>
+console.log("array<number>")
+console.log(listaDeNumerosdos);
