cf45a8a75560baec4c7b423a4e402c91c69f454a
alejos4n <alejo-lopera@hotmail.com>
2025-09-24 00:22:04 -0500

    tipo any

---
+++ tiposany.js
@@ -0,0 +1,7 @@
+/**usar any hace que se pierdan los beneficios del tipado
+la idea de usar typescript es por el tipado.
+Por lo cual se recomienda usar any en casos estrictamente necesarios
+*/
+var variablesintipo = "Hola Lector";
+variablesintipo = 100;
+console.log(variablesintipo);
---
+++ tiposany.ts
@@ -0,0 +1,12 @@
+/**usar any hace que se pierdan los beneficios del tipado
+la idea de usar typescript es por el tipado. 
+Por lo cual se recomienda usar any en casos estrictamente necesarios
+*/
+
+
+let variablesintipo: any = "Hola Lector";
+variablesintipo = 100;
+
+console.log(variablesintipo);
+
+// como se ve, recibe un string e imprime un number
