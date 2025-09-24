980f3b09ea4e793cf65a62f5d52d22d653830667
alejos4n <alejo-lopera@hotmail.com>
2025-09-24 00:28:03 -0500

    tipo unkwon

---
+++ tiposunkhown.ts
@@ -0,0 +1,10 @@
+//tipos unknown
+
+//cuando se necesita describir un valor no conodio que viene por ejemplo de una API
+
+//lo que indica es que a futuro la variable puede tomar cualquier valor
+
+let valorDesconocido: unknown = 4;
+valorDesconocido = true;
+
+//no genera errores en el compilador
