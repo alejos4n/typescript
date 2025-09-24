cca78791cc3d5641828c04db3ac47228d2ae4a1f
alejos4n <alejo-lopera@hotmail.com>
2025-09-23 23:45:23 -0500

    tuplas

---
+++ tipostuplas.ts
@@ -0,0 +1,13 @@
+//tupla son como un arreglo pero con un número conocido de elementos que no reuqieren ser los mismos
+
+let futbolista: [string, number];
+futbolista = ["Luchito", 28];
+
+//imprimimos la tupla
+console.log(futbolista);
+
+//accediendo a los elementos de la tupla
+//usamos su indice
+
+console.log(`el nombre es ${futbolista[0]}`);
+console.log(`Su edad es ${futbolista[1]}`)
