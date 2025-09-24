16d2d724dcffac2e46a23f0afb923733837907c0
alejos4n <alejo-lopera@hotmail.com>
2025-09-24 00:32:41 -0500

    tipo void

---
+++ tiposvoid.ts
@@ -0,0 +1,10 @@
+//void es el opuesto a  any
+//any quiere decir cualqueir tipo, void representa ningún tipo
+//no es muy util pues solo se le pueden asignar valores null o undefined
+
+function saludar(): void {
+    console.log("Hola mundo");
+}
+
+//ejecutamos la función lo que imprimirá Hoal mundo
+saludar();
