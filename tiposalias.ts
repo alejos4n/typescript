47a097ecf2738fe9ca53f9f05931d9521ef1bd1e
alejos4n <alejo-lopera@hotmail.com>
2025-09-30 15:54:41 -0500

    tipos alias

---
+++ tiposalias.ts
@@ -0,0 +1,23 @@
+//los tipos alias es un nombre dado a cualquier tipo que se utilizan varias veces
+
+//se crea el alias, en este caso tipo number pero no quiere decir que sean siempre le mismo tipo
+type Punto = {
+    x: number;
+    y: number;
+};
+
+function imprimirCoordenada(punto: Punto) {
+    console.log(`La coordenada x es: ${punto.x}`);
+    console.log(`La coordenmada y es: ${punto.y}`);
+}
+
+imprimirCoordenada({x: 10, y: 25});
+
+//alias de texto
+type TextoMayusculas = string;
+
+function convertirMayusculas(texto: string): TextoMayusculas {
+    return texto.toUpperCase();
+}
+
+console.log(convertirMayusculas("hola mundo typescript"))
