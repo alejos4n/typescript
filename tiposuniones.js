97c17c2b55316cd12b6bef7bfde27a6231c71ce1
alejos4n <alejo-lopera@hotmail.com>
2025-09-30 11:08:54 -0500

    metodos de uniones

--- tiposuniones.js
+++ tiposuniones.js
@@ -8,3 +8,21 @@ function imprimirId(id) {
 }
 imprimirId(1); //pasamos un valor númerico
 imprimirId("cadena texto"); // pasamos un valor de texto
+/**El resultado será:
+ *  El id es 1
+    El id es cadena texto
+ 
+* ahora usaremos los métodos asociados a id, o sea, number y string.
+* como cada tipo tiene diferentes métodos, se validan luego
+* Ahora vamos a cambiar a mayúsculas cuando sea un string y a reducir el número de decimales del number
+*/
+function imprimirId2(id) {
+    if (typeof id === "string") {
+        console.log("El id es ".concat(id.toUpperCase())); //pasamos a mayúsculas
+    }
+    else {
+        console.log("El id es ".concat(id.toFixed(2)));
+    }
+}
+imprimirId2("este es mi id");
+imprimirId2(100.546556438);
--- tiposuniones.ts
+++ tiposuniones.ts
@@ -14,6 +14,28 @@ imprimirId(1); //pasamos un valor númerico
 imprimirId("cadena texto"); // pasamos un valor de texto
 
 /**El resultado será:
- * El id es 1
+ *  El id es 1
     El id es cadena texto
+ 
+* ahora usaremos los métodos asociados a id, o sea, number y string.
+* como cada tipo tiene diferentes métodos, se validan luego
+* Ahora vamos a cambiar a mayúsculas cuando sea un string y a reducir el número de decimales del number
+*/
+
+function imprimirId2(id: number | string) {
+    if (typeof id === "string") {
+        console.log(`El id es ${(id as string).toUpperCase()}`); //pasamos a mayúsculas
+    } else {
+        console.log(`El id es ${(id as number).toFixed(2)}`);
+    }
+}
+
+imprimirId2("este es mi id");
+imprimirId2(100.546556438); 
+
+
+/**el resultado final es 
+ * 
+ * El id es ESTE ES MI ID
+ * El id es 100.55
  */
